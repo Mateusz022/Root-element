3e280d9901fe2980fdfeb0d7c7763b5600c12e70
Mateusz <100945821+Mateusz022@users.noreply.github.com>
2022-04-17 20:13:32 +0200

    Add files via upload

---
+++ Root-element.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pierwiastek_samodzielnie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double epsilon = 0.000001;
+            double a,
+            x = 1;
+            Console.WriteLine("podaj wartość zmiennej a");
+            a = Convert.ToDouble(Console.ReadLine());
+            if (a < 0)
+            {
+                Console.WriteLine("zgłaszam wyjątek", a); //throw new exception
+            } while (Math.Abs(x * x - a) > epsilon)
+            {
+                x = (x + a / x) / 2;
+            }
+            Console.WriteLine("Wartością (przybliżoną) pierwiastka kwadratowego liczby {0} jest {1}.", a, x);
+            Console.WriteLine("Wartością pierwiastka kwadratowego liczby {0} jest {1}.", a, Math.Sqrt(a));
+            Console.ReadKey();
+
+        }
+    }
+}
